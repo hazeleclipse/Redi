3c7112d4b9f3cdd2767812afc0662283873f8a8b
alphaEmpyrean <76848679+alphaEmpyrean@users.noreply.github.com>
2023-07-19 13:15:25 -0400

    Add Staker Get and Create Api Endpoints
    Created minimal apis for the Stakers Get and Create functionality. Added in a Requests Namespace to hold the potential requests coming into the system

--- Redi.MinimalApi/Program.cs
+++ Redi.MinimalApi/Program.cs
@@ -1,6 +1,44 @@
+using MediatR;
+using Redi.Application.Extensions;
+using Redi.Application.Stakers.Commands.Register;
+using Redi.Application.Stakers.Queries.GetById;
+using Redi.Infrastructure.Extensions;
+using Redi.MinimalApi.Dto;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var stakers = app.MapGroup("/api/stakers");
+
+stakers.MapPost("/", CreateStaker);
+stakers.MapGet("/{id}", GetStakerById);
 
 app.Run();
+
+static async Task<IResult> CreateStaker(CreateStakerRequest request, ISender mediatr)
+{
+    var createStaker = new RegisterStaker(
+        Email: request.Email,
+        FirstName: request.FirstName,
+        LastName: request.LastName,
+        Password: request.Password,
+        Role: request.Role);
+
+    var newStakerDto = await mediatr.Send(createStaker);
+    
+    return TypedResults.Created(String.Empty, newStakerDto);
+}
+
+static async Task<IResult> GetStakerById(Guid id, ISender mediatr)
+{
+    var getStakerById = new GetStakerById(id);
+
+    var stakerDto = await mediatr.Send(getStakerById);
+
+    return TypedResults.Ok(stakerDto);
+}
---
+++ Redi.MinimalApi/Requests/CreateStakerRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Redi.MinimalApi.Dto
+{
+    public record CreateStakerRequest(
+        string Email,
+        string FirstName,
+        string LastName,
+        string Password,
+        string Role);
+}
