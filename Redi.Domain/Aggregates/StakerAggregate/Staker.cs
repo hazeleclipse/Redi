0e4890b115e2756e01b86c760fb531923ec5b2b3
alphaEmpyrean <76848679+alphaEmpyrean@users.noreply.github.com>
2023-07-20 15:36:16 -0400

    Refactor references to pruned staker attributes in Staker Queries
    Removed references to Stakers Attributes that no longer exist from the Staker Query objects.

--- Redi.Domain/Aggregates/StakerAggregate/Staker.cs
+++ Redi.Domain/Aggregates/StakerAggregate/Staker.cs
@@ -1,5 +1,4 @@
 ﻿using Redi.Domain.Aggregates.StakerAggregate.ValueObjects;
-using Redi.Domain.Common.Enumerations;
 using Redi.Domain.Common.Models;
 
 namespace Redi.Domain.Aggregates.StakerAggregate;
@@ -7,12 +6,11 @@ namespace Redi.Domain.Aggregates.StakerAggregate;
 public class Staker : AggregateRoot<StakerId>
 {
 
-
     public Staker() { }
 
     private Staker(StakerId id) : base(id) { }
 
     public static Staker Create(StakerId id)
-        => new Staker(id);
+        => new(id);
 
 }
