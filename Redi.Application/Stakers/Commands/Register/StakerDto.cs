efd8ec0621b0b497c683ea99c43a820afa685f4b
alphaEmpyrean <76848679+alphaEmpyrean@users.noreply.github.com>
2023-07-20 15:42:40 -0400

    Refactor references to pruned staker attributes in Staker Commands
    Removed references to Stakers Attributes that no longer exist from the Staker Command objects. This includes removing the Edit command as you are not able to edit the StakerId once it is set.

--- Redi.Application/Stakers/Commands/Register/StakerDto.cs
+++ Redi.Application/Stakers/Commands/Register/StakerDto.cs
@@ -1,9 +1,4 @@
 ﻿namespace Redi.Application.Stakers.Commands.Register
 {
-    public record StakerDto(
-        Guid Id,
-        string Email,
-        string FirstName,
-        string LastName,
-        string Role);
+    public record StakerDto(Guid Id);
 }
