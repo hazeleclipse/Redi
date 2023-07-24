870167b2c713d59254d1afece16fece72304f2ce
alphaEmpyrean <76848679+alphaEmpyrean@users.noreply.github.com>
2023-07-24 18:19:42 -0400

    Add in NodeId value object

---
+++ Redi.Domain/Aggregates/NodeAggregate/ValueObjects/NodeId.cs
@@ -0,0 +1,31 @@
+﻿using Redi.Domain.Common.Models;
+
+namespace Redi.Domain.Aggregates.NodeAggregate.ValueObjects
+{
+    public class NodeId : ValueObject
+    {
+        public NodeId() { }
+
+        public Guid Value { get; }
+
+        private NodeId(Guid id)
+            => Value = id;
+
+        public static NodeId Create(Guid id)
+            => new(id);
+
+        public static implicit operator NodeId(Guid id)
+            => new(id);
+
+        public static implicit operator Guid(NodeId id)
+            => id.Value;
+
+        public static implicit operator Guid?(NodeId? id)
+        => id?.Value;
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
