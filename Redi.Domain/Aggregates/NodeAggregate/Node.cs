d47d5aab479346e6edbef4338438057df7fd32f8
alphaEmpyrean <76848679+alphaEmpyrean@users.noreply.github.com>
2023-07-24 18:29:16 -0400

    Add in Node abstract class

---
+++ Redi.Domain/Aggregates/NodeAggregate/Node.cs
@@ -0,0 +1,17 @@
+﻿using Redi.Domain.Aggregates.NodeAggregate.ValueObjects;
+using Redi.Domain.Common.Models;
+using Redi.Domain.Common.ValueObjects;
+
+namespace Redi.Domain.Aggregates.NodeAggregate
+{
+    public abstract class Node : AggregateRoot<NodeId>
+    {
+        public string Name { get; private set; } = default!;
+        public Node? Parent { get; private set; }
+        public Stake Stake { get; private set; } = 0;
+        public Stake LocalStake { get; private set; } = 0;
+        public Weight Weight { get; private set; } = 1;
+
+        public Node() { }
+    }
+}
