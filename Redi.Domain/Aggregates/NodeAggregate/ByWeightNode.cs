be7ccbf52207cb57431ff82e5b3712e16315d98a
alphaEmpyrean <76848679+alphaEmpyrean@users.noreply.github.com>
2023-07-26 12:24:43 -0400

    Add in ByWeightNode root entity

---
+++ Redi.Domain/Aggregates/NodeAggregate/ByWeightNode.cs
@@ -0,0 +1,19 @@
+﻿using Redi.Domain.Aggregates.NodeAggregate.Entities;
+using Redi.Domain.Aggregates.NodeAggregate.ValueObjects;
+
+namespace Redi.Domain.Aggregates.NodeAggregate
+{
+    public class ByWeightNode : Node
+    {
+        readonly HashSet<StakerWeightEntry> _weights = new()
+            ;
+        public ByWeightNode() { }
+
+        private ByWeightNode(NodeId id, string name, CoreNode? parent) : base(id, name, parent) { }
+
+        public static ByWeightNode Create(NodeId id, string name, CoreNode? parent)
+            => new(id, name, parent);
+
+        public IReadOnlyCollection<StakerWeightEntry> Weights => _weights;
+    }
+}
