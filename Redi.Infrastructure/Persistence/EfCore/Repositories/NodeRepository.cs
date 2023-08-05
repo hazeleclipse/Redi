7287e7659e5584de790c574e337e8a30caed9baa
alphaEmpyrean <76848679+alphaEmpyrean@users.noreply.github.com>
2023-08-05 13:00:01 -0400

    Add NodeRepository implimentation

---
+++ Redi.Infrastructure/Persistence/EfCore/Repositories/NodeRepository.cs
@@ -0,0 +1,23 @@
+﻿using Redi.Application.Persistence;
+using Redi.Domain.Aggregates.NodeAggregate;
+
+namespace Redi.Infrastructure.Persistence.EfCore.Repositories
+{
+    internal class NodeRepository : INodeRepository
+    {
+        private readonly RediDbContext _rediDbContext;
+
+        public NodeRepository(RediDbContext rediDbContext)
+            => _rediDbContext = rediDbContext;
+
+        public void Add(Node container)
+        {
+            if (_rediDbContext.Nodes.Contains(container))
+                return;
+
+            _rediDbContext.Nodes.Add(container);
+
+            _rediDbContext.SaveChanges();
+        }
+    }
+}
