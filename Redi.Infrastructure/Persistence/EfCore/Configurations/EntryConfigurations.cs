ede6f49c9a54002120dc593050d2b4c65c30f790
alphaEmpyrean <76848679+alphaEmpyrean@users.noreply.github.com>
2023-08-05 10:58:31 -0400

    Add Staker Weight Entry EFCore Config

---
+++ Redi.Infrastructure/Persistence/EfCore/Configurations/EntryConfigurations.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Redi.Domain.Aggregates.NodeAggregate.Entities;
+using Redi.Domain.Aggregates.NodeAggregate.ValueObjects;
+using Redi.Domain.Aggregates.StakerAggregate.ValueObjects;
+using Redi.Domain.Common.ValueObjects;
+
+namespace Redi.Infrastructure.Persistence.EfCore.Configurations
+{
+    internal class WeightEntryConfigurations : IEntityTypeConfiguration<StakerWeightEntry>
+    {
+        public void Configure(EntityTypeBuilder<StakerWeightEntry> builder)
+        {
+            builder.ToTable(nameof(StakerWeightEntry));
+
+            builder.HasKey(sm => sm.Id);
+
+            builder.Property(sm => sm.Id)
+                .HasConversion(
+                    id => id.Value,
+                    value => EntryId.Create(value));
+
+            builder.Property(sm => sm.NodeId)
+                .HasConversion(
+                    id => id.Value,
+                    value => NodeId.Create(value));
+
+            builder.Property(sm => sm.StakerId)
+                .HasConversion(
+                    id => id.Value,
+                    value => StakerId.Create(value));
+
+            builder.Property(sm => sm.Stake)
+                .HasConversion(
+                    stake => stake.Value,
+                    value => new Stake(value));
+
+            builder.Property(sm => sm.LocalStake)
+                .HasConversion(
+                    localStake => localStake.Value,
+                    value => new Stake(value));
+
+            builder.Property(sm => sm.Weight)
+                .HasConversion(
+                    weight => weight.Value,
+                    value => new Weight(value));
+
+            builder.HasIndex(sm => sm.StakerId);
+        }
+    }
+}
