287384e907d3e7658815bd8adfe953f91bdc1d30
alphaEmpyrean <76848679+alphaEmpyrean@users.noreply.github.com>
2023-08-06 12:50:23 -0400

    Add Create and GetAll Node api request handlers

---
+++ Redi.MinimalApi/Nodes/NodeHanlder.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Redi.Application.Nodes.Commands.Create;
+using Redi.Application.Nodes.Queries.GetAll;
+
+namespace Redi.MinimalApi.Nodes
+{
+    internal static class NodeHanlder
+    {
+        internal static async Task<IResult> CreateNode(CreateNodeRequest request, ISender mediatr)
+        {
+            var createNode = new CreateNode(Name: request.Name, NodeType: request.NodeType);
+
+            var nodeDto = await mediatr.Send(createNode);
+
+            return TypedResults.Created($"/api/nodes/{nodeDto.Id}", nodeDto);
+        }
+
+        internal static async Task<IResult> GetAllNodes(ISender mediatr)
+        {
+            var getAllNodes = new GetAllNodes();
+
+            var nodeDtos = await mediatr.Send(getAllNodes);
+
+            return TypedResults.Ok(nodeDtos);
+        }
+    }
+}
