836b750f97d4ba09638ecde45d7c86126337da7d
Josh <76848679+alphaEmpyrean@users.noreply.github.com>
2023-08-26 15:43:29 -0500

    Allow tests to see Redi.MinimalApi Internal classes

---
+++ src/Redi.MinimalApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+// Internals are visable to tests
+[assembly: InternalsVisibleToAttribute("Redi.MinimalApi.Tests.Unit")]
+[assembly: InternalsVisibleToAttribute("Redi.MinimalApi.Tests.Integration")]
