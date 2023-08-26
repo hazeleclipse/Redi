51af1fb08d0c82e7cf9d58ffa8f049486c5aa9e6
Josh <76848679+alphaEmpyrean@users.noreply.github.com>
2023-08-26 15:45:08 -0500

    Add test class for Node routes

---
+++ test/Redi.MinimalApi.Tests.Unit/NodeRoutesTests.cs
@@ -0,0 +1,12 @@
+using Redi.MinimalApi;
+
+namespace Redi.MinimalApi.Tests.Unit;
+
+public class NodeRoutesTests
+{
+    [Fact]
+    public void GetById_EntityExists_Return200OkAndSpecifiedNode()
+    {
+        
+    }
+}
